91e271250388acb8889c0d08d7ceb40507a6d710
LiWeny16 <454888395@qq.com>
2022-10-31 17:34:54 +0800

    update

---
+++ greasyfork_script/config.js
@@ -0,0 +1,18 @@
+var picArr = new Array
+    //自行添加图片url，调用方法，输入数字下标即可 如输入：2
+    picArr = [
+        //bing每日壁纸 索引号0
+
+        "https://bing.biturl.top/?resolution=1920&format=image&index=0&mkt=zh-CN",//动态每日壁纸
+        "https://bing.com/th?id=OHR.NationalDay2022_ZH-CN3861603311_1920x1080.jpg",
+        "https://bing.com/th?id=OHR.BridgeofSighs_ZH-CN5414607871_1920x1080.jpg",
+        //Wallpaper Abyss壁纸 索引号3
+        "https://images4.alphacoders.com/171/171916.jpg",
+        "https://images5.alphacoders.com/613/613927.jpg",
+        "https://images2.alphacoders.com/606/606275.jpg",
+        "https://images2.alphacoders.com/742/742320.png",
+        //添加你自己的壁纸...... 索引号7
+        "https://dogefs.s3.ladydaily.com/~/source/wallhaven/full/8o/wallhaven-8o2dpj.png?w=2560&fmt=webp",
+        "https://bing.biturl.top/?resolution=1920&format=image&index=0&mkt=zh-CN",
+        "nothing"
+    ]
