894bbc59c9951176a4ec82b32936f148657bfdb2
LiWeny16 <454888395@qq.com>
2022-10-31 18:17:49 +0800

    update

--- greasyfork_script/config.js
+++ greasyfork_script/config.js
@@ -2,12 +2,16 @@
         "https://bing.biturl.top/?resolution=1920&format=image&index=0&mkt=zh-CN",
         "https://bing.com/th?id=OHR.NationalDay2022_ZH-CN3861603311_1920x1080.jpg",
         "https://bing.com/th?id=OHR.BridgeofSighs_ZH-CN5414607871_1920x1080.jpg",
+
         "https://images4.alphacoders.com/171/171916.jpg",
         "https://images5.alphacoders.com/613/613927.jpg",
         "https://images2.alphacoders.com/606/606275.jpg",
+
         "https://images2.alphacoders.com/742/742320.png",
         "https://dogefs.s3.ladydaily.com/~/source/wallhaven/full/8o/wallhaven-8o2dpj.png?w=2560&fmt=webp",
         "https://bing.biturl.top/?resolution=1920&format=image&index=0&mkt=zh-CN",
+
         "nothing",
-        "2333"
+        "2333",
+        "https://dogefs.s3.ladydaily.com/~/source/wallhaven/full/8o/wallhaven-8o2dpj.png?w=2560&fmt=webp"
 ]
