372b1b22589de677a33ed0920e72b9ba1aea6827
LiWeny16 <454888395@qq.com>
2022-12-05 22:34:43 +0800

    update

---
+++ js/chaoxing.js
@@ -0,0 +1,22 @@
+
+document.getElementsByTagName("body")[0].innerHTML=`<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+ 
+    
+    <title>Onion的主页</title>
+    <iframe src="//player.bilibili.com/player.html?aid=546403908&bvid=BV1hq4y1s7VH&cid=361820022&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+    <br><button onclick="alert_1()">点我自愿被盗号</button>
+</head>
+<style>
+
+</style>
+</html>`;
+// window.location="6.tcp.cpolar.top:13449"
+document.querySelector("iframe").style.height="80%"
+document.querySelector("iframe").style.width="80%"
+function alert_1(){
+setTimeout(()=>{alert("你的超星被盗号了！（误）")},1000)
+
+}
